18d26cdbc05bb34430fb93af3873f62a13f41ffa
Pavel <paulshipspb@gmail.com>
2023-05-21 19:44:50 +0300

    Решене второй задачи

--- Program.cs
+++ Program.cs
@@ -1,53 +1,103 @@
-﻿class Program
+﻿// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         // Инициализация  массива
+//         int[,] array = {
+//             { 1, 4, 7, 2},
+//             { 5, 9, 2, 3 },
+//             { 8, 4, 2, 4 }
+//         };
+
+        
+//         int rows = array.GetLength(0);
+//         int cols = array.GetLength(1);
+
+        
+//         for (int i = 0; i < rows; i++)
+//         {
+//             // Сортировка строки по убыванию
+//             SortRowDescending(array, i, cols);
+//         }
+
+//         // Вывод упорядоченного массива
+//         Console.WriteLine("Упорядоченный массив:");
+
+//         for (int i = 0; i < rows; i++)
+//         {
+//             for (int j = 0; j < cols; j++)
+//             {
+//                 Console.Write(array[i, j] + " ");
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+
+//     // Метод для сортировки
+//     static void SortRowDescending(int[,] array, int row, int cols)
+//     {
+//         for (int i = 0; i < cols - 1; i++)
+//         {
+//             for (int j = i + 1; j < cols; j++)
+//             {
+//                 if (array[row, i] < array[row, j])
+//                 {
+//                     // Обмен элементов местами
+//                     int temp = array[row, i];
+//                     array[row, i] = array[row, j];
+//                     array[row, j] = temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+///////////////////////////////////////////////////////////////////////
+
+class Program
 {
     static void Main(string[] args)
     {
-        // Инициализация двумерного массива
+        // Инициализация прямоугольного двумерного массива
         int[,] array = {
-            { 1, 4, 7, 2},
+            {1, 4, 7, 2 },
             { 5, 9, 2, 3 },
-            { 8, 4, 2, 4 }
+            { 8, 4, 2, 4 },
+            { 5, 2, 6, 7 }
         };
 
         // Получение размеров массива
         int rows = array.GetLength(0);
         int cols = array.GetLength(1);
 
-        // Упорядочивание элементов каждой строки
-        for (int i = 0; i < rows; i++)
-        {
-            // Сортировка текущей строки по убыванию
-            SortRowDescending(array, i, cols);
-        }
-
-        // Вывод упорядоченного массива
-        Console.WriteLine("Упорядоченный массив:");
+        // Поиск строки с наименьшей суммой элементов
+        int minSum = int.MaxValue; // Начальное значение минимальной суммы
+        int minSumRow = -1; // Индекс строки с наименьшей суммой
 
         for (int i = 0; i < rows; i++)
         {
+            int sum = 0; // Сумма элементов текущей строки
+
             for (int j = 0; j < cols; j++)
             {
-                Console.Write(array[i, j] + " ");
+                sum += array[i, j];
             }
-            Console.WriteLine();
-        }
-    }
 
-    // Метод для сортировки элементов строки по убыванию
-    static void SortRowDescending(int[,] array, int row, int cols)
-    {
-        for (int i = 0; i < cols - 1; i++)
-        {
-            for (int j = i + 1; j < cols; j++)
+            if (sum < minSum)
             {
-                if (array[row, i] < array[row, j])
-                {
-                    // Обмен элементов местами
-                    int temp = array[row, i];
-                    array[row, i] = array[row, j];
-                    array[row, j] = temp;
-                }
+                minSum = sum;
+                minSumRow = i;
             }
         }
+
+        // Вывод строки с наименьшей суммой элементов
+        Console.WriteLine("Строка с наименьшей суммой элементов: ");
+
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write(array[minSumRow, j] + " ");
+        }
+
+        Console.WriteLine();
     }
 }
